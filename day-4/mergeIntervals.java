03656dbdaf9e17068adf629576703fa12832cf30
JaySingh <jaysingh1452@gmail.com>
2023-06-14 23:22:21 +0530

    edited mergeIntervals

--- day-4/mergeIntervals.java
+++ day-4/mergeIntervals.java
@@ -4,45 +4,37 @@ public class mergeIntervals {
     public static void main(String[] args) {
         try(Scanner sc = new Scanner(System.in)) {
             int[][] arr = {{1, 3}, {8, 10}, {2, 6}, {15, 18}};
-            List<List<Integer>> ans = mergeOverlappingIntervals(arr);
+            int[][] ans = mergeOverlappingIntervals(arr);
             System.out.print("The merged intervals are: \n");
-            for (List<Integer> it : ans) {
-                System.out.print("[" + it.get(0) + ", " + it.get(1) + "] ");
+            for (int[] it : ans) {
+                System.out.print("[" + it[0] + ", " + it[1] + "] ");
             }
             System.out.println();
         }
     }
 
-    static List<List<Integer>> mergeOverlappingIntervals(int[][] arr) {
-        int n = arr.length;
-        Arrays.sort(arr, new Comparator<int[]>() {
-            public int compare(int[] a, int[] b) {
-                return a[0] - b[0];
+    static int[][] mergeOverlappingIntervals(int[][] arr) {
+       
+        List<int[]> res = new ArrayList<>();
+        if(arr.length == 0)
+            return null;
+        
+        //sort the array in ascending order
+        Arrays.sort(arr , (a, b) -> a[0] - b[0]);
+
+        int mergedInterval[] = arr[0];
+
+        for(int[] interval : arr) {
+            if(interval[0] <= mergedInterval[1])
+                mergedInterval[1] = Math.max(interval[1], mergedInterval[1]);
+            else {
+                res.add(mergedInterval);
+                mergedInterval = interval;
             }
-        });
-
-        List<List<Integer>> ans = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) { // select an interval:
-            int start = arr[i][0];
-            int end = arr[i][1];
-
-            //Skip all the merged intervals:
-            if (!ans.isEmpty() && end <= ans.get(ans.size() - 1).get(1)) {
-                continue;
-            }
-
-            //check the rest of the intervals:
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j][0] <= end) {
-                    end = Math.max(end, arr[j][1]);
-                } else {
-                    break;
-                }
-            }
-            ans.add(Arrays.asList(start, end));
         }
-        return ans;
+
+        res.add(mergedInterval);
+        return res.toArray(new int[res.size()][]);
         
     }
 }
